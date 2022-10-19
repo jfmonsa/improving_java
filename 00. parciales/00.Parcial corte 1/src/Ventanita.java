3f1fdcb1a8716d3eb388fe517386b68a508c02b7
jfmonsalve <juanmonsalve@protonmail.com>
2022-10-19 18:46:42 -0500

    solved error

--- 00. parciales/00.Parcial corte 1/src/Ventanita.java
+++ 00. parciales/00.Parcial corte 1/src/Ventanita.java
@@ -7,20 +7,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-
 public class Ventanita extends JFrame implements ActionListener, ItemListener{
-    Container contenedor=getContentPane();
-    JButton btn_mañana, btn_tarde;
-    JPanel panel_ppal, panel_mañana, panel_tarde;
-    int ancho_ventana =400;
-    int largo_ventana =450;
-    JComboBox<String> panel_mañana_c1;
-    JButton panel_mañana_btn_terminar, panel_mañana_btn_agregar, panel_mañana_btn_regresar;
-    JRadioButton rbtn1, rbtn2;
-    ButtonGroup jornada_btngroup;
-    JTextArea panel_mañana_tArea;
-
-    //Parte logica xd
+    //------------------------------ Global vars ---------------------------------------
     //Array de las materias
     String c1 = materiasMatriculadas.Fisíca.name();
     String c2 = materiasMatriculadas.Español.name();
@@ -29,135 +17,97 @@ public class Ventanita extends JFrame implements ActionListener, ItemListener{
     String arr_materias[] = {c1,c2,c3,c4};
     //static String arr_materias[] = {"Fisíca","Español","Matemáticas","Sociales"};
     static ArrayList<AsignarMatricula> materias_matriculadas = new ArrayList<AsignarMatricula>();
-
-    //Profesores
     //Materias por la mañana
-        //String[] arr_mat1 = {"Fisica","Matematicas"}; //Las da prof1
-        //String[] arr_mat4 = {"Español","Sociales"}; //Las da prof4
-
+    //String[] arr_mat1 = {"Fisica","Matematicas"}; //Las da prof1
+    //String[] arr_mat4 = {"Español","Sociales"}; //Las da prof4
     //Materias por la tarde
-        //String[] arr_mat2 = {"Fisica"}; //Las da prof2
-        //String[] arr_mat3 = {"Matematicas"}; //Las da prof 3
-        //String[] arr_mat5 = {"Español","Sociales"}; //Las da prof 5
-
+    //String[] arr_mat2 = {"Fisica"}; //Las da prof2
+    //String[] arr_mat3 = {"Matematicas"}; //Las da prof 3
+    //String[] arr_mat5 = {"Español","Sociales"}; //Las da prof 5
+    
     profHoraCatedra prof1 = new profHoraCatedra("Ramiro Ramirez","123456789","Ing Sistemas", 8);
     profHoraCatedra prof2 = new profHoraCatedra("Lexmer antonio","012378965","Agronomo", 8);
     profNombrado prof3 = new profNombrado("Yonio jimenez", "159623487",Categorias.Asociado, "01/02/1752");
     profNombrado prof4 = new profNombrado("Matecho restrepo", "487159623",Categorias.Auxiliar, "01/02/1502");
     profNombrado prof5 = new profNombrado("Jorge Orwell", "159487623",Categorias.Asistente, "01/02/1984");
-
     
+    //------------------------------ Global vars ---------------------------------------
+    //------------------------------ GUI vars ---------------------------------------
+    Container contenedor;
+    //JPanel contenedor;
+    int ancho_ventana =400;
+    int largo_ventana =450;
+    JComboBox<String> combo1;
+    JButton btn_terminar, btn_agregar, btn_regresar;
+    JRadioButton rbtn1, rbtn2;
+    ButtonGroup btngroup1;
+    JTextArea textArea;
+    //------------------------------ GUI vars ---------------------------------------
 
+    //Window constructor
     public Ventanita(){
         super("Interfaz de colegio");
-        this.setSize(400,450);
-        this.setVisible(true);
+        contenedor=getContentPane();
+        FlowLayout layout = new FlowLayout();
+        contenedor.setLayout(layout);
+        //Other settings
+        setSize(ancho_ventana,largo_ventana);
+        setBackground(Color.LIGHT_GRAY);
+        setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBackground(Color.LIGHT_GRAY);
-        this.setResizable(false);
-        this.setLayout(null);
-        
 
-        //ventana principal
-        panel_ppal = new JPanel();
-        panel_ppal.setBounds(0,0,ancho_ventana,largo_ventana);
-        contenedor.add(panel_ppal);
+        //--------------------------- Form ---------------------------------
+        JLabel label1  = new JLabel("Matricular jornada mañana");
+        label1.setFont(new Font("Segoe UI Black", 0, 24));
+        contenedor.add(label1);
 
+        JLabel label_combo1 = new JLabel("Seleccione materia a matricular");
+        contenedor.add(label_combo1);
 
-        JLabel l1  = new JLabel("Intefaz colegio");
-        l1.setFont(new Font("Segoe UI Black", 0, 24));
-        l1.setBounds(100,10,200,50); //SetLocationRelative(null)
-        panel_ppal.add(l1);
+        combo1= new JComboBox<>(arr_materias);
+        contenedor.add(combo1);
 
+        JLabel label_radio_btngroup = new JLabel("Seleccione la jornada");
+        contenedor.add(label_radio_btngroup);
 
-        JLabel l2  = new JLabel("Seleccione un jornada:    ");
-        l2.setBounds(20,50,200,50); //SetLocationRelative(null)
-        panel_ppal.add(l2);
-
-        btn_mañana = new JButton("Mañana");
-        btn_mañana.setBounds(20,110,100,40);
-        btn_mañana.addActionListener(this);
-        panel_ppal.add(btn_mañana);
-
-        /*btn_tarde = new JButton("Tarde");
-        btn_tarde.setBounds(260,110,100,40);
-        btn_tarde.addActionListener(this);
-        panel_ppal.add(btn_tarde);*/
-
-        //JPanel mañana
-        panel_mañana = new JPanel();
-        panel_mañana.setBounds(0,0,ancho_ventana,largo_ventana);
-        //panel_mañana.setBackground(Color.red);
-        panel_mañana.setBounds(0,0,400,450);
-        panel_mañana.setVisible(false);
-        contenedor.add(panel_mañana);
-
-        JLabel panel_mañana_l1  = new JLabel("Matricular jornada mañana");
-        panel_mañana_l1.setFont(new Font("Segoe UI Black", 0, 24));
-        //panel_mañana_l1.setBounds(100,10,200,50); //SetLocationRelative(null)
-        panel_mañana.add(panel_mañana_l1);
-
-        JLabel panel_mañana_l2 = new JLabel("Seleccione materia a matricular");
-        //panel_mañana_l2.setBounds(20,50,200,50); //SetLocationRelative(null)
-        panel_mañana.add(panel_mañana_l2);
-
-        
-        panel_mañana_c1= new JComboBox<>(arr_materias);
-        //panel_mañana_c1.setBounds(200,110,100,40);
-        panel_mañana.add(panel_mañana_c1);
-
-        JLabel l7 = new JLabel("Seleccione la jornada");
-        panel_mañana.add(l7);
         rbtn1= new JRadioButton("Mañana",true);
         rbtn2= new JRadioButton("Tarde");
-        jornada_btngroup = new ButtonGroup();
-        jornada_btngroup.add(rbtn1);
-        jornada_btngroup.add(rbtn2);
-        panel_mañana.add(rbtn1);
-        panel_mañana.add(rbtn2);
+        btngroup1 = new ButtonGroup();
+        btngroup1.add(rbtn1);
+        btngroup1.add(rbtn2);
+        contenedor.add(rbtn1);
+        contenedor.add(rbtn2);
 
-        panel_mañana_tArea= new JTextArea(5,30);
-        panel_mañana_tArea.setBounds(200,280,100,40);
-        panel_mañana_tArea.setEditable(false);
-        panel_mañana_tArea.setFocusable(false);
-        panel_mañana_tArea.setLineWrap(true);
-        panel_mañana_tArea.setWrapStyleWord(true);
-        panel_mañana.add(panel_mañana_tArea);
-        JScrollPane scroll=new JScrollPane(panel_mañana_tArea/* , JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED*/);
-        panel_mañana.add(scroll);
+        textArea = new JTextArea(5,30);
+        textArea.setBounds(200,280,100,40);
+        textArea.setEditable(false);
+        textArea.setFocusable(false);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        contenedor.add(textArea);
+        JScrollPane scroll=new JScrollPane(textArea/* , JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED*/);
+        contenedor.add(scroll);
    
+        btn_agregar = new JButton("Agregar materia");
+        btn_agregar.addActionListener(this);
+        contenedor.add(btn_agregar);
 
-        panel_mañana_btn_agregar = new JButton("Agregar materias");
-        //panel_mañana_btn_agregar.setBounds(20,380,100,40);
-        panel_mañana_btn_agregar.addActionListener(this);
-        panel_mañana.add(panel_mañana_btn_agregar);
+        btn_terminar = new JButton("Terminar de matricular");
+        btn_terminar.addActionListener(this);
+        contenedor.add(btn_terminar);   
+        //--------------------------- Form ---------------------------------
 
-        panel_mañana_btn_terminar = new JButton("Terminar de matricular");
-        //panel_mañana_btn_terminar.setBounds(260,380,100,40);
-        panel_mañana_btn_terminar.addActionListener(this);
-        panel_mañana.add(panel_mañana_btn_terminar);
-
-
-        panel_mañana_btn_regresar = new JButton("Regresar al menu");
-        panel_mañana_btn_regresar.addActionListener(this);
-        panel_mañana_btn_regresar.setBackground(Color.DARK_GRAY);
-        panel_mañana_btn_regresar.setForeground(Color.CYAN);
-        panel_mañana_btn_regresar.setBorder(BorderFactory.createLineBorder(Color.CYAN, 2)); //Poniendole un borde al boton
-        panel_mañana.add(panel_mañana_btn_regresar);
-
-        //JPanel tarde
+        //####################################################
+        this.setVisible(true); 
+            //this always goes to the end, otherwise it doesn't work
+        //####################################################
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==btn_mañana){
-            panel_ppal.setVisible(false);
-            panel_mañana.setVisible(true);
-        }
-
-        //Para agregar materias a la matricula
-        if(e.getSource()==panel_mañana_btn_agregar){ 
-            String materia_seleccionada = (String) panel_mañana_c1.getSelectedItem();
+        //To add subjects
+        if(e.getSource()==btn_agregar){ 
+            String materia_seleccionada = (String) combo1.getSelectedItem();
             String jornada_seleccionada="";
             if(rbtn1.isSelected()){
                 jornada_seleccionada="Mañana";
@@ -167,9 +117,10 @@ public class Ventanita extends JFrame implements ActionListener, ItemListener{
             Matricular(jornada_seleccionada,materia_seleccionada);
 
         }
-        //Para terminar la matricula
-        if(e.getSource()==panel_mañana_btn_terminar){
 
+        //to finish tuition process
+        if(e.getSource()==btn_terminar){
+            verificar2Materias(materias_matriculadas);
         }
     }
     @Override
@@ -230,23 +181,47 @@ public class Ventanita extends JFrame implements ActionListener, ItemListener{
             AsignarMatricula matricula;
             matricula = new AsignarMatricula(jornada,prof,materia/*jornada_seleccionada, arr_p5, materia_seleccionada*/);
             materias_matriculadas.add(matricula);
-            panel_mañana_tArea.setText(actualizarTabla());
+            textArea.setText(actualizarTabla());
         }else{
-            JOptionPane.showMessageDialog(panel_mañana, "La materia que solicitas ya fue matricualada","Warning",JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(contenedor, "La materia que solicitas ya fue matricualada","Warning",JOptionPane.WARNING_MESSAGE);
         }
     }
-    /*public void profesorConMasMaterias(){
+    public void profesorConMasMaterias(){
         Profesor[] arr_profesores = {prof1,prof2,prof3,prof4,prof5};
-        int valor_inicial=0;
-        for(Profesor p: arr_profesores){
-            for(AsignarMatricula m: materias_matriculadas){
+        int[] arr_conunter_profesores = new int[arr_profesores.length];
+        int i=0; //indexer
 
+        for(Profesor p: arr_profesores){ 
+            int counter=0;   
+            for(AsignarMatricula m: materias_matriculadas){
+                if(p.getCedula()==m.getProfesor()[0].getCedula()){
+                    counter++;
+                }
+            }
+            arr_conunter_profesores[i]=counter;
+            i++;
+        }
+
+        //iterate arr_conunter_profesores to find the index of the busiest prof
+        int max_val=arr_conunter_profesores[0];
+        int j=0;
+        int final_index=0;
+        System.out.println("jljadflkjadfjljk");
+        for(; j<arr_profesores.length-1; j++){
+            if(arr_conunter_profesores[j]>max_val){
+                max_val=arr_conunter_profesores[j];
+                final_index=j;
             }
         }
-    }*/
+        JOptionPane.showMessageDialog(contenedor, "El profesor con más materias matriculadas es: "+arr_profesores[final_index].getNombre()+" , da "+max_val+" materia(s)");
+        JOptionPane.showMessageDialog(contenedor, "Matricula exitosa programa a terminar");
+        System.exit(0);
+    }
     public void verificar2Materias(ArrayList<AsignarMatricula> arr){
-        if(!(arr.size()<2)){
-            JOptionPane.showMessageDialog(panel_mañana, "Debe matricular por lo menos dos materias", "Warning", JOptionPane.WARNING_MESSAGE);
+        if(arr.size()<2){
+            JOptionPane.showMessageDialog(contenedor, "Debe matricular por lo menos dos materias", "Warning", JOptionPane.WARNING_MESSAGE);
+        }else{
+            profesorConMasMaterias();
         }
     }
 }
