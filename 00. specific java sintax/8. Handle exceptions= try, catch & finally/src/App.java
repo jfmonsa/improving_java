493548b398b821b890f442838c6fb0d7ae152ff9
jfmonsalve <juanmonsalve@protonmail.com>
2022-11-03 10:10:55 -0500

    haciendo ultimo taller

--- 00. specific java sintax/8. Handle exceptions= try, catch & finally/src/App.java
+++ 00. specific java sintax/8. Handle exceptions= try, catch & finally/src/App.java
@@ -7,9 +7,9 @@ public class App {
 		
 		// exception = 	an event that occurs during the execution of a program that,
 		//				disrupts the normal flow of instructions
-        
-		Scanner scanner = new Scanner(System.in);
+        Scanner scanner = null;
 		while (true){
+			scanner = new Scanner(System.in);
 			try {
 				System.out.println("Enter a whole number to divide: ");
 				int x = scanner.nextInt();
@@ -18,7 +18,7 @@ public class App {
 				int z = x/y;
 				System.out.println("result: " + z);
 			}
-			catch(ArithmeticException e) {
+			catch(ArithmeticException e) {	
 				System.out.println("You can't divide by zero! IDIOT!");
 				/*
 				 * El objeto e de la clase ArithmeticException tiene tambien varios metodos
@@ -33,12 +33,35 @@ public class App {
 			catch(Exception e) {
 				//Default exception
 				System.out.println("Something went wrong");
+				// No hacer es mala practica xd
 			}
 			finally {
 				//This allways is excecuted
 				scanner.close();
+				scanner=null;
+				System.gc();
 			}
 		}
-			
 	}
+
+	/**
+	 * Otra forma de mandar exepciones es con  los metodos agregandoles throws y las exepciones
+	 * esto le indicara al programador que cuando invoque este método se debe invocar dentro de
+	 * un try -> catch por que el método puede arrojar estas exepciones
+	 * 
+	 * Entonces para nuestro ejemplo cuando invoquemos el método calcular(), debe ser así:
+	 * 
+		try {
+			calcular();
+		} catch (NumberFormatException e) {
+			// code...
+		}
+		} catch (ArithmeticException e) {
+			// code...
+		}
+	 */
+	public void division() throws NumberFormatException, ArithmeticException{
+		//Code...
+	}
+	
 }
