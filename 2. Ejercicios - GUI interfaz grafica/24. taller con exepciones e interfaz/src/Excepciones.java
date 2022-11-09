1711d92e969a01e08db8b1ab51d22fdf33381d2f
jfmonsalve <juanmonsalve@protonmail.com>
2022-11-09 18:40:49 -0500

    Last excersice again (24)

--- 2. Ejercicios - GUI interfaz grafica/24. taller con exepciones e interfaz/src/Excepciones.java
+++ 2. Ejercicios - GUI interfaz grafica/24. taller con exepciones e interfaz/src/Excepciones.java
@@ -5,4 +5,9 @@ public class Excepciones extends Exception{
         JOptionPane.showMessageDialog(null, message, "Excepcion:", JOptionPane.ERROR_MESSAGE);
         //super(mensaje);
     }
+    public Excepciones(String message, int constant){
+        JOptionPane.showMessageDialog(null, message, "Excepcion:", constant);
+        //super(mensaje);
+    }
+    //throw new Excepciones("Error: Debes seleccionar al menos una pelicula");
 }
